a9344f4799708cfe4e1e3f66d5a0c17535118012
Thomas Sten <eric@Thomass-MBP.fios-router.home>
2021-01-05 11:16:17 -0500

    apiconfig

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,18 @@
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const user = await verifyUser()
+      user ? setUser(user) : setUser(null)
+    }
+    fetchUser()
+  }, [])
+
+  const clearUser = () => setUser(null)
+  
   return (
     <div className="App">
 
