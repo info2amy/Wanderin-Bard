50ad82e8e56e0e6227c2fdd608a8a4dbf7f35129
David Verghese <dj.verghese@gmail.com>
2021-01-06 14:26:03 -0500

    adjusted styling of signup component

--- client/src/screens/SignUp/SignUp.jsx
+++ client/src/screens/SignUp/SignUp.jsx
@@ -1,106 +1,113 @@
-import React, { useState } from 'react'
-import './SignUp.css'
-import { signUp, signIn } from '../../services/users'
-import { useHistory } from "react-router-dom"
+import React, { useState } from "react";
+import "./SignUp.css";
+import { signUp, signIn } from "../../services/users";
+import { useHistory } from "react-router-dom";
 
 const SignUp = (props) => {
+  const history = useHistory();
 
-    const history = useHistory()
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+    passwordConfirmation: "",
+    isError: false,
+    errorMsg: "",
+  });
 
-    const [form, setForm] = useState({
-        username: '',
-        email: '',
-        password: '',
-        passwordConfirmation: '',
-        isError: false,
-        errorMsg: ''
-    })
+  const handleChange = (event) =>
+    setForm({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
 
-    const handleChange = event =>
+  const onSignUp = (event) => {
+    event.preventDefault();
+    const { setUser } = props;
+
+    signUp(form)
+      .then(() => signIn(form))
+      .then((user) => setUser(user))
+      .then(() => history.push("/"))
+      .catch((error) => {
+        console.error(error);
         setForm({
-            ...form,
-            [event.target.name]: event.target.value
-        })
+          email: "",
+          password: "",
+          passwordConfirmation: "",
+          isError: true,
+          errorMsg: "Sign Up Details Invalid",
+        });
+      });
+  };
 
-    const onSignUp = event => {
-        event.preventDefault()
-        const { setUser } = props
-
-        signUp(form)
-            .then(() => signIn(form))
-            .then(user => setUser(user))
-            .then(() => history.push('/'))
-            .catch(error => {
-                console.error(error)
-                setForm({
-                    email: '',
-                    password: '',
-                    passwordConfirmation: '',
-                    isError: true,
-                    errorMsg: 'Sign Up Details Invalid'
-                })
-            })
+  const renderError = () => {
+    const toggleForm = form.isError ? "danger" : "";
+    if (form.isError) {
+      return (
+        <button type="submit" className={toggleForm}>
+          {form.errorMsg}
+        </button>
+      );
+    } else {
+      return (
+        <button id="buttonId" type="submit">
+          Sign Up
+        </button>
+      );
     }
+  };
 
-    const renderError = () => {
-        const toggleForm = form.isError ? 'danger' : ''
-        if (form.isError) {
-            return (
-                <button type="submit" className={toggleForm}>
-                    {form.errorMsg}
-                </button>
-            )
-        } else {
-            return <button type="submit">Sign Up</button>
-        }
-    }
+  const { email, username, password, passwordConfirmation } = form;
 
-    const { email, username, password, passwordConfirmation } = form
-
-    return (
-        <div className="form-container">
-            <h3>Sign Up</h3>
-            <form onSubmit={onSignUp}>
-                <label>Username</label>
-                <input
-                    required
-                    type="text"
-                    name="username"
-                    value={username}
-                    placeholder="Enter username"
-                    onChange={handleChange}
-                />
-                <label>Email address</label>
-                <input
-                    required
-                    type="email"
-                    name="email"
-                    value={email}
-                    placeholder="Enter email"
-                    onChange={handleChange}
-                />
-                <label>Password</label>
-                <input
-                    required
-                    name="password"
-                    value={password}
-                    type="password"
-                    placeholder="Password"
-                    onChange={handleChange}
-                />
-                <label>Password Confirmation</label>
-                <input
-                    required
-                    name="passwordConfirmation"
-                    value={passwordConfirmation}
-                    type="password"
-                    placeholder="Confirm Password"
-                    onChange={handleChange}
-                />
-                {renderError()}
-            </form>
+  return (
+    <div className="form-container">
+      <h3 id="signup-h3">Sign Up</h3>
+      <form onSubmit={onSignUp}>
+        <label>Username</label>
+        <input
+          className="signup-input"
+          required
+          type="text"
+          name="username"
+          value={username}
+          placeholder="Enter username"
+          onChange={handleChange}
+        />
+        <label>Email address</label>
+        <input
+          className="signup-input"
+          required
+          type="email"
+          name="email"
+          value={email}
+          placeholder="Enter email"
+          onChange={handleChange}
+        />
+        <label>Password</label>
+        <input
+          className="signup-input"
+          required
+          name="password"
+          value={password}
+          type="password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
+        <label>Password Confirmation</label>
+        <input
+          className="signup-input"
+          required
+          name="passwordConfirmation"
+          value={passwordConfirmation}
+          type="password"
+          placeholder="Confirm Password"
+          onChange={handleChange}
+        />
+        {renderError()}
+        </form>
         </div>
-    )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
