43cc8c375c8e1f2efd91d608a52c564940f5541d
Gylychmyrat Mammedov <gylychmyratmammedov@GylychmyratsMBP.amity.org>
2021-01-11 10:12:45 -0500

    styling for sort button

--- client/src/components/Sort/Sort.jsx
+++ client/src/components/Sort/Sort.jsx
@@ -8,7 +8,15 @@ const Sort = (props) => {
 
   return (
     <div>
-      
+        <form className="sort-container" onSubmit={props.handleSubmit}>
+            <label htmlFor="sort">SORT BY:</label>
+            <select className="sort" onChange={handleChange}>
+                <option className="option" value="name-ascending" >&nbsp; Alphabetically, A-Z &nbsp;</option>
+                <option value="name-descending">&nbsp; Alphabetically, Z-A &nbsp;</option>
+                <option value="price-ascending">&nbsp; Price, low to high &nbsp;</option>
+                <option value="price-descending">&nbsp; Price, high to low &nbsp;</option>
+            </select>
+        </form>
     </div>
   );
 };
